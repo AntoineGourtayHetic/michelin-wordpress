f0f62b8a1217ad369904645123cd3a0fe6bf0b95
Luca <haenell@yahoo.fr>
2016-10-20 15:58:11 +0200

    major update

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <header id="header">
+        <!-- Tout le contenu de l entête de mon site -->
+            <h1>Michelin 2017!</h1>
+        </header>
