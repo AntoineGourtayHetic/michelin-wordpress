18f0dccb274d369df9ef6f17a41a91eeef22eb19
Antoine Gourtay <antoinegourtay@MacBook-Air-de-Antoine.local>
2016-10-28 16:07:36 +0200

    Changes header

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ if (have_posts()){
     while (have_posts()){
         the_post();
 ?>
-    <!-- <div class="cd-timeline-block">
+    <div class="cd-timeline-block">
         <div class="cd-timeline-img cd-picture">
             <img src="<?php bloginfo('template_url'); ?>/build/img/cd-icon-picture.svg" alt="Picture">
         </div> <!-- cd-timeline-img -->
 
-        <!-- <div class="cd-timeline-content">
+        <div class="cd-timeline-content">
             <h2><?php the_title(); ?></h2>
             <p><?php the_excerpt(); ?></p>
             <a href="<?php the_permalink(); ?>" class="cd-read-more">Read more</a>
