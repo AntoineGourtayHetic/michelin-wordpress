ca9cb33067d1f1527c3a96c4a6aa42c1e01ed496
Philippe Henoch <henoch.philippe@gmail.com>
2016-11-03 08:49:45 +0100

    Modify contact template

--- page-templates/single-contact.php
+++ page-templates/single-contact.php
@@ -13,10 +13,9 @@ if (have_posts()){
         the_post();
 ?>
             <h2><?php the_title(); ?></h2>
-            <p><?php the_content(); ?></p>
-            <a href="<?php the_permalink(); ?>" class="cd-read-more">Read more</a>
-            <span class="cd-date"><?php the_time('d/m/Y'); ?></span>
-						<?php echo do_shortcode(); ?>
+								<?php the_content(); ?>
+							</div>
+						</div>
 <?php
     }
 }
