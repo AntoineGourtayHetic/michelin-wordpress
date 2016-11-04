83b151f14fdfc3711e7b14c35241a15695d570b4
Philippe Henoch <henoch.philippe@gmail.com>
2016-11-04 16:15:08 +0100

    Fix <header> and header menu

--- header.php
+++ header.php
@@ -7,21 +7,23 @@
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
-    <nav>
-        <div class="nav-wrapper">
-        <div class="right"><?php echo do_shortcode( '[paypal_donation_button]' ); ?></div>
-        <a href="<?php echo home_url();?>" class="left logo"><img src="<?php bloginfo('template_url'); ?>/build/img/logo.svg" alt="michelin_logo" width="40px" /><span>2017</span></a>
-            <ul id="nav-mobile" class="left hide-on-small-and-down">
-                <?php $args=array(
-                  'theme_location' => 'header', // nom du slug
-                  'menu' => 'header_fr', // nom à donner cette occurence du menu
-                  'menu_class' => 'menu_header', // class à attribuer au menu
-                  'menu_id' => 'menu_id', // id à attribuer au menu
-                  // voir les autres arguments possibles sur le codex
-                  );
-                  wp_nav_menu($args); ?>
-            </ul>
-        </div>
-    </nav>
+		<header id="header">
+	    <nav>
+	        <div class="nav-wrapper">
+	        <div class="right"><?php echo do_shortcode( '[paypal_donation_button]' ); ?></div>
+	        <a href="<?php echo home_url();?>" class="left logo"><img src="<?php bloginfo('template_url'); ?>/build/img/logo.svg" alt="michelin_logo" width="40px" /><span>2017</span></a>
+	            <ul id="nav-mobile" class="left hide-on-small-and-down">
+	                <?php $args=array(
+	                  'theme_location' => 'header', // nom du slug
+	                  'menu' => 'header_fr', // nom à donner cette occurence du menu
+	                  'menu_class' => 'menu_header', // class à attribuer au menu
+	                  'menu_id' => 'menu_id', // id à attribuer au menu
+	                  // voir les autres arguments possibles sur le codex
+	                  );
+	                  wp_nav_menu($args); ?>
+	            </ul>
+	        </div>
+	    </nav>
+		</header>
 
     <div class="container">
