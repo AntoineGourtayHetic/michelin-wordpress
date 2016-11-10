624ba0ada03300c87a136c30b4bdd4e34db32340
Philippe Henoch <henoch.philippe@gmail.com>
2016-11-10 11:56:38 +0100

    Add Google Maps in contact template

--- page-templates/single-contact.php
+++ page-templates/single-contact.php
@@ -6,6 +6,7 @@ Template Name: Contact
 <?php get_header(); ?>
 
 <section>
+<iframe width="100%" height="250" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:EjsyMyBQbGFjZSBkZXMgQ2FybWVzIERlY2hhdXgsIDYzMDAwIENsZXJtb250LUZlcnJhbmQsIEZyYW5jZQ&key=AIzaSyAme2CIVxtDe1NXGWKzOSfJNnVf9jUjasQ" allowfullscreen></iframe>
 <?php
 // boucle WordPress
 if (have_posts()){
