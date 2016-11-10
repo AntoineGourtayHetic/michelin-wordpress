3082675d5c3ee56db762c2b0ed83a8c55c123f5b
Philippe Henoch <henoch.philippe@gmail.com>
2016-11-10 12:28:24 +0100

    Add Google Maps without API key

--- page-templates/single-contact.php
+++ page-templates/single-contact.php
@@ -6,7 +6,7 @@ Template Name: Contact
 <?php get_header(); ?>
 
 <section>
-<iframe width="100%" height="250" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=place_id:EjsyMyBQbGFjZSBkZXMgQ2FybWVzIERlY2hhdXgsIDYzMDAwIENsZXJtb250LUZlcnJhbmQsIEZyYW5jZQ&key=AIzaSyAme2CIVxtDe1NXGWKzOSfJNnVf9jUjasQ" allowfullscreen></iframe>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2625.3531066821515!2d2.4185486155927314!3d48.85147667928686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e67283047127a9%3A0x5a597564e51389ac!2sHETIC!5e0!3m2!1sen!2sfr!4v1478776732877" width="100%" height="250" frameborder="0" style="border:0" allowfullscreen></iframe>
 <?php
 // boucle WordPress
 if (have_posts()){
