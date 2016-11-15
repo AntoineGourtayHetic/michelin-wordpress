a5ee8d18c9cd975b005d01fdf720062e715812c0
Philippe Henoch <henoch.philippe@gmail.com>
2016-11-15 13:23:56 +0100

    Add responsive page contatc

--- single-contact.php
+++ single-contact.php
@@ -16,10 +16,10 @@ if (have_posts()){
 
             <h2><?php the_title(); ?></h2>
 						<div class="row">
-							<div class="col s6">
+							<div class="col s12 m6">
 									<?php echo do_shortcode(get_option('contact_form')); ?>
 							</div>
-							<div class="col s6">
+							<div class="col s12 m6">
 									<?php the_content(); ?>
 							</div>
 						</div>
